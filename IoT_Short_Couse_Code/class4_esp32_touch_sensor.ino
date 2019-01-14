5308e8cba10cc2fde2fedc7b91c91efe7fc17dac
Syed Razwanul Haque Nabil <nabilsust@gmail.com>
2019-01-14 18:27:57 +0600

    Update and rename class4_debounce.ino to class4_esp32_touch_sensor.ino

---
+++ IoT_Short_Couse_Code/class4_esp32_touch_sensor.ino
@@ -0,0 +1,34 @@
+/***************************************************** 
+* ESP32 Touch Test and LED Ctrl
+* Touch pin ==> Touch0 is T0 which is on GPIO 4 (D4).
+* LED pin   ==> D2
+
+*****************************************************/
+
+#define TOUTCH_PIN T0 // ESP32 Pin D4
+#define LED_PIN 2
+int touch_value = 100;
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(100); // give me time to bring up serial monitor
+  Serial.println("ESP32 Touch Test");
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite (LED_PIN, LOW);
+}
+
+void loop()
+{
+  touch_value = touchRead(TOUTCH_PIN);
+  Serial.println(touch_value);  // get value using T0 
+  if (touch_value < 50)
+  {
+    digitalWrite (LED_PIN, HIGH);
+  }
+  else
+  {
+    digitalWrite (LED_PIN, LOW);
+  }
+  delay(100);
+}
